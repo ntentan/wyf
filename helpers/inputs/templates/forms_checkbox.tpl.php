f7ad93b565ac755ffe4d1774825dd0ebad4de25a
Ekow Abaka Ainooson <jainooson@gmail.com>
2013-02-10 01:57:31 +0000

    Checkboxes now have a negative value

--- helpers/inputs/templates/forms_checkbox.tpl.php
+++ helpers/inputs/templates/forms_checkbox.tpl.php
@@ -1,1 +1,4 @@
-<label><input type="checkbox" value="1" <?= $value == '1' ? 'checked="checked"' : '' ?> <?= $attributes ?>/> <?= $label ?></label>
+<label>
+    <input type="hidden" value="0" name="<?= $name ?>" />
+    <input type="checkbox" value="1" <?= $value == '1' ? 'checked="checked"' : '' ?> <?= $attributes ?>/> <?= $label ?>
+</label>
