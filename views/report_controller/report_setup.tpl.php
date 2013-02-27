ffefe0a899fef68ff5f9d524fddfcd0d161e22f7
Ekow Abaka Ainooson <jainooson@gmail.com>
2013-02-27 00:23:52 +0000

    Still trying to beef up the reporting engine

--- views/report_controller/report_setup.tpl.php
+++ views/report_controller/report_setup.tpl.php
@@ -1,17 +1,42 @@
 <h2><?= $report_title ?></h2>
+<?php 
+$helpers->form->getRendererInstance()->noWrap = true;
+?>
 <?= $helpers->form->open()->action($action_route)->attribute("target", "_blank") ?>
 <div id="wyf-report-wrapper">
     <div id="wyr-report-options">
         <h3>Filters</h3>
         <?php foreach($report_filters as $title => $filter): ?>
         <div>
-        <span><?= $title ?></span>
+            <span><?= $title ?></span>
+            <?php 
+            switch($filter['type'])
+            {
+                case DataSource::TYPE_FLOAT:
+                case DataSource::TYPE_INTEGER:
+                    echo $helpers->form->get_selection_list('')
+                        ->option("is equal to")
+                        ->option("is greater than")
+                        ->option("is less than")
+                        ->option("is between")
+                        ->option("is not equal to")
+                        ->option("is not between");
+                    break;
+                case DataSource::TYPE_TEXT:
+                    echo $helpers->form->get_selection_list('')
+                        ->option("matches")
+                        ->option("contains")
+                        ->option("is less than")
+                        ->option("is between");                    
+                    break;
+            }
+            ?>
         </div>
         <?php endforeach; ?>
     </div>
     <div id="wyf-report-output">
     <?=
-    $helpers->form->get_selection_list('Output')->option('Portable Document Format', 'pdf')
+    $helpers->form->get_selection_list('Output', 'output')->option('Portable Document Format', 'pdf')
     ?>
     </div>
 </div>
