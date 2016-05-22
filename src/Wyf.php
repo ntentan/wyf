4ced6548648c2e570dc529a6451a76bc6a795d9b
Ekow Abaka Ainooson <jainooson@gmail.com>
2016-05-22 14:57:21 +0000

    Fixed issue with faulty routes

--- src/Wyf.php
+++ src/Wyf.php
@@ -82,7 +82,7 @@ class Wyf
                 'controller_path' => substr("$controllerPath/$path", 1)
             ];
         } else {
-            return [];
+            throw new \ntentan\exceptions\RouteNotAvailableException();
         }
     }
 }
