698503bea7ae5bedf48dd9293c578dc3eafe9645
James Ainooson <jainooson@gmail.com>
2017-04-01 17:43:14 -0500

    Relocating the position of the description and error messages

--- views/forms/forms_layout.tpl.php
+++ views/forms/forms_layout.tpl.php
@@ -9,18 +9,16 @@
     <?php if(!$hideWrapper): ?><div class="form-element <?= $errors ? "form-error" : "" ?>" id="form-element-<?= $id ?>"><?php endif; ?>
     <?php if($label): ?><label><?= $label ?></label><?php endif; ?>
     <?= unescape($element) ?>
-    <?php if($description != ''): ?>
-        <span class="form-description"><?= $description ?></span>
-    <?php endif; ?>
     <?php if(count($errors)): ?>
-        <div class="form-error">
         <ul>
             <?php foreach($errors as $error): ?>
             <li><?= $error ?></li>
             <?php endforeach; ?>
         <?php ?>
         </ul>
-        </div>
+    <?php endif; ?>
+    <?php if($description != ''): ?>
+        <span class="form-description"><?= $description ?></span>
     <?php endif; ?>
     <?php if(!$hideWrapper): ?></div><?php endif; ?>
 <?php endforeach; ?>
